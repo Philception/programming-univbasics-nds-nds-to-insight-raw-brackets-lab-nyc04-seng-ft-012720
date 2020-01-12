aa8b1c07ba174d3816683f040daa32c454482a56
Phillip Henry III <philtrader71@gmail.com>
2020-01-12 18:27:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,7 +19,7 @@ def directors_totals(nds)
     movie_index = 0
 
       while movie_index < nds[directors_index][:movies].length do
-        totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+        totals[director_name] += nds[directors_index][:movies][movie_index][:worldwide_gross]
         movie_index += 1
       end
       directors_index
